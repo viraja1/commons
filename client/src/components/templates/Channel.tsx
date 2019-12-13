b8896049045ad8f2831ba099154d59ec2b2e42a0
Matthias Kretschmann <m@kretschmann.io>
2019-12-13 12:06:40 +0100

    bump to react-scripts v3.3
    * allows to bump all eslint packages too
    * fix new lint errors

--- client/src/components/templates/Channel.tsx
+++ client/src/components/templates/Channel.tsx
@@ -81,13 +81,16 @@ export default class Channel extends PureComponent<ChannelProps, ChannelState> {
         }
     }
 
-    private handlePageClick = async (data: { selected: number }) => {
+    private onPageClick = async (data: { selected: number }) => {
         // react-pagination starts counting at 0, we start at 1
         const toPage = data.selected + 1
 
         this.props.history.push({ search: `?page=${toPage}` })
 
-        await this.setState({ currentPage: toPage, isLoading: true })
+        this.setState({
+            currentPage: toPage,
+            isLoading: true
+        })
         await this.getChannelAssets()
     }
 
@@ -119,7 +122,7 @@ export default class Channel extends PureComponent<ChannelProps, ChannelState> {
                     <Pagination
                         totalPages={totalPages}
                         currentPage={currentPage}
-                        handlePageClick={this.handlePageClick}
+                        handlePageClick={this.onPageClick}
                     />
                 </Content>
             </Route>
